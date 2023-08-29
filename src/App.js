da9387376e69017ea99cffadcc3661bfdcaabe8c
lawrahkonwea <lawrahcollections@gmail.com>
2023-08-29 14:22:51 +0100

    Import and route files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-// import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Homepage from './Pages/Homepage';
 import NavBar from './Pages/NavBar';
 import Signup from './Pages/Signup';
 
-const App = () => {
-  return (
-   <>
-   <NavBar />
-   <Routes>
-    <Route path='/' element={<Homepage/>} />
-    <Route path='/Signup' element={<Signup />} />
-   </Routes>
-   </>
-  );
-}
+const App = () => (
+  <>
+
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  </>
+);
 
 export default App;
