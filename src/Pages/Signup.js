fc73fd53b6e5ac750026e8bf41f2d0dc1c6c56e0
lawrahkonwea <lawrahcollections@gmail.com>
2023-08-29 14:21:02 +0100

    simple signup modal

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,21 +1,25 @@
-import React, { useState } from 'react'
-// import { CSSTransition } from 'react-transition-group';
+import React from 'react';
+// import { Link } from 'react-router-dom/dist'
+import Homepage from './Homepage';
+import './Signup.css';
 
 const Signup = () => {
-  const [showSignUp, setShowSignUp] = useState(false);
-
+  const handleModal = () => {
+    window.history.go(-1);
+  };
 
   return (
     <div>
-    <button onClick={() => setShowSignUp(true)}> signup </button>
-    {/* <CSSTransition
-        in={showSignUp}
-        timeout={300}
-        classNames="fade"
-        unmountOnExit
-      ></CSSTransition> */}
+      <Homepage />
+      <div className="show-signup">
+        <div className="signup-container">
+          <button type="button" style={{ marginLeft: 'auto' }} onClick={handleModal}>X</button>
+          {/* <Link to="/" style={{marginLeft: 'auto'}}>X</Link>  This method can also work  */}
+          <h4>Hello Amaka! I am from signup</h4>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
