287243c42793f0293669017d15fc60578c6fe0e3
lawrahkonwea <lawrahcollections@gmail.com>
2023-08-29 16:27:32 +0100

    Make changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './Pages/Homepage';
 import NavBar from './Pages/NavBar';
-import Signup from './Pages/Signup';
 
-const App = () => (
-  <>
+const App = () => {
+  const [active, setActive] = useState(false);
 
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
-  </>
-);
+  const handleClick = () => {
+    setActive(true);
+    window.history.replaceState(null, 'signup', '/signup');
+  };
+
+  return (
+    <>
+      <NavBar handleClick={handleClick} />
+      <Routes>
+        <Route path="/" element={<Homepage active={active} setActive={setActive} />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Signup from './Signup';
 
-const Homepage = () => (
+const Homepage = ({ active, setActive }) => (
+  <>
+    <div className="home">
+      <h1>Homepage</h1>
 
-  <div className="home">
-    <h1>Homepage</h1>
-
-  </div>
+    </div>
+    {active && <Signup setActive={setActive} />}
+  </>
 );
+
+Homepage.propTypes = {
+  active: PropTypes.bool.isRequired,
+  setActive: PropTypes.func.isRequired,
+};
 
 export default Homepage;
